441cad869b249351d49c8d94940871178cd36c0b
josemaria038 <josemaria1036@gmail.com>
2025-02-07 00:58:08 -0500

    Initial commit

---
+++ ws-test/src/test/java/us/taller/tech/test/ws_test/WsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package us.taller.tech.test.ws_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
