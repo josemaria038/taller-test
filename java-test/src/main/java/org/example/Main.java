3d7824ad03eb08d2f17537e5369584888b99646d
josemaria038 <josemaria1036@gmail.com>
2025-02-07 10:06:21 -0500

    Taller test

--- java-test/src/main/java/org/example/Main.java
+++ java-test/src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("First example: " + getSubstringsFrom("abcc", 2));
+    System.out.println("Second example: " + getSubstringsFrom("aabab", 3));
   }
+
+  /**
+   * Count of substrings of length K with exactly K distinct characters
+   * Given string str of the lowercase alphabet and an integer K, the task
+   * is to count all substrings of length K which have exactly K distinct characters.
+   *
+   * @param str Input
+   * @param k   Length
+   * @return
+   */
+  public static int getSubstringsFrom(String str, int k) {
+    int result = 0;
+    String sub;
+    for (int i = 0; i < str.length() - k; i++) {
+      sub = str.substring(i, i + k);
+      if (areCharactersDistinct(sub)) {
+        result++;
+      }
+    }
+
+    return result;
+  }
+
+  public static boolean areCharactersDistinct(String s) {
+    List<Character> list = s.chars()
+        .mapToObj(c -> (char) c)
+        .toList();
+    Set<Character> set = new HashSet<>(list);
+    return set.size() == s.length();
+  }
+
 }
